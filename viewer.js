90423957a67818fade0284eebf66a6c87c8af9d8
josh900 <jm5522@sbcglobal.net>
2024-08-25 09:39:30 -0700

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -141,7 +141,7 @@ function renderPage(num) {
 
         // Add this new code
         setTimeout(() => {
-            sendMessageToAvatar("<speak> Hello there, <break strength=\"medium\"/> feel free to ask me anything about SKOOP, <break strength=\"medium\"/> I'll try my best to answer correctly. </speak>");
+            sendMessageToAvatar("<speak> Hello there, <break strength="medium"/> feel free to ask me anything about <phoneme alphabet="ipa" ph="skuːp">Skoop</phoneme><break strength="medium"/>, I'll try my best to answer correctly. </speak>");
         }, 5000);
     }
 
